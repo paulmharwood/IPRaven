07d688d6b1487c33922c6caab2951573ffbd8e2f
Paul <paul.harwood@fujitsu.com>
2020-10-23 14:41:50 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IpComponent } from './ip/ip.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IpComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/ip/ip.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ip',
+  templateUrl: './ip.component.html',
+  styleUrls: ['./ip.component.css']
+})
+export class IpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
