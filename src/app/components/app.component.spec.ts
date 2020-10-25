bf0e15508eabfba94a74caa8ead7416b65e6c013
Paul <paul.harwood@fujitsu.com>
2020-10-25 09:03:01 +0000

    Declared ip history module in testbed for app component spec

--- src/app/components/app.component.spec.ts
+++ src/app/components/app.component.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { IpHistoryComponent } from './ip-history/ip-history.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AppComponent],
+      declarations: [AppComponent, IpHistoryComponent],
     }).compileComponents();
   });
 
